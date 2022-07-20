e7bd79af112204ce494e7075d5e2ff46dd86ab4b
holic <474944802@qq.com>
2022-07-20 15:28:32 +0800

    urls

--- clamp/bookmanager/book/views.py
+++ clamp/bookmanager/book/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpRequest
+
 from django.http import HttpResponse
+
 def index(request):
 
     return HttpResponse("ok")
